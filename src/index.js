1e86eb88becefbe34f5ba6b6f5d42d32bd972666
William Costa <dev.williamcosta@gmail.com>
2017-07-19 23:11:58 -0300

    Created first React component.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Webpack funcionando.')
+'use strict'
+
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Title = require('./app')
+
+ReactDOM.render(
+    React.createElement(Title),
+    document.querySelector('[data-js="app"]')
+)
