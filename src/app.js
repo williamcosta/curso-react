258052799bc86d33592a7dd1574988cf9e7bcd82
William Costa <dev.williamcosta@gmail.com>
2017-07-24 21:06:35 -0300

    Add configuration for the HMR working with ReactJS.

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@
 
 import React from 'react'
 
-var Title = React.createClass({
-    render: function () {
-        return <h1>Título</h1>
-    }
+const App = React.createClass({
+  render: function () {
+    return <h1>Aplicação</h1>
+  }
 })
 
-export default Title
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 'use strict'
 
-import React  from 'react'
-import ReactDOM from 'react-dom'
-import Title from './app'
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './app'
 
-
-ReactDOM.render(
-    <Title />,
+const renderApp = (NextApp) => {
+  render(
+    <AppContainer>
+      <NextApp />
+    </AppContainer>,
     document.querySelector('[data-js="app"]')
-)
+  )
+}
+
+renderApp(App)
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app').default
+
+    renderApp(NextApp)
+  })
+}
