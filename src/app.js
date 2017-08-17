35cf3cd819f305119b0f33d71dd894ed5a5f9441
William Costa <dev.williamcosta@gmail.com>
2017-08-16 23:51:16 -0300

    Add disabled search input when submit.

--- src/app.js
+++ src/app.js
@@ -24,8 +24,10 @@ class App extends Component {
     const value = e.target.value
     const keyCode = e.which || e.keyCode
     const ENTER = 13
+    const target = e.target
 
     if (keyCode === ENTER) {
+      target.disabled = true
       ajax().get(this.getGitHubApiUrl(value))
         .then((result) => {
           this.setState({
@@ -40,6 +42,9 @@ class App extends Component {
             repos: [],
             starred: []
           })
+        })
+        .always(() => {
+          target.disabled = false
         })
     }
   }
